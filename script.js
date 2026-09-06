c395b192f43d533059c832a33aedd8c56f33194d
martinhe25 <kunzeh@andrew.cmu.edu>
2026-09-05 23:34:21 -0400

    first pass landing

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// Website interactivity will be added here.
+const landingScreen = document.querySelector("#landing");
+const portfolioScreen = document.querySelector("#portfolio");
+const enterButton = document.querySelector("#enter-button");
+const backButton = document.querySelector("#back-button");
+const portfolioTitle = document.querySelector("#portfolio-title");
+const transitionDuration = 300;
+
+function showScreen(screenToShow, screenToHide) {
+    screenToHide.classList.remove("is-active");
+    screenToHide.setAttribute("aria-hidden", "true");
+    screenToHide.inert = true;
+
+    screenToShow.classList.add("is-active");
+    screenToShow.removeAttribute("aria-hidden");
+    screenToShow.inert = false;
+}
+
+enterButton.addEventListener("click", () => {
+    showScreen(portfolioScreen, landingScreen);
+
+    window.setTimeout(() => {
+        portfolioTitle.focus();
+    }, transitionDuration);
+});
+
+backButton.addEventListener("click", () => {
+    showScreen(landingScreen, portfolioScreen);
+
+    window.setTimeout(() => {
+        enterButton.focus();
+    }, transitionDuration);
+});
